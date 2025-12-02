a00d39fa65af94c3cc0390d149440a0538f67fcd
TomKord <tom.kordes1@gmail.com>
2025-12-02 14:11:56 +0100

    Some model updates

--- Script.R
+++ Script.R
@@ -1,4 +1,5 @@
 #install.packages("gamlss.add")
+#install.packages("gamlss")
 
 library(gamlss)
 library(gamlss.add)
@@ -18,13 +19,11 @@ hist(df$acc_precip)
 
 d1 <- subset(df, acc_precip > 0 & acc_precip < 10)
 
-d1$acc_precip
-
 hist(d1$acc_precip, breaks = 50)
 
 
 
-zaga <- gamlss(
+zaga <- gamlss( # Zero-Adjusted-Gamma
   acc_precip ~ mean_temp + mean_relative_hum + mean_wind_speed +
     bright_sunshine + mean_pressure,
   
@@ -41,3 +40,34 @@ summary(zaga)
 wp(zaga, xvar = df$mean_temp)
 
 vcov(zaga)
+
+df$y_hat <- predict(zaga, type = "response")
+
+df$y_hat
+
+
+quantile(df$acc_precip, c(0.25,0.5,0.75, 0.8, 0.85, 0.9, 0.95))
+quantile(df$y_hat, c(0.25,0.5,0.75, 0.8, 0.85, 0.9, 0.95))
+
+
+
+zaga2 <- gamlss(
+  acc_precip ~ mean_temp + I(mean_temp^2) +
+    mean_relative_hum + I(mean_relative_hum^2) +
+    mean_temp:mean_relative_hum +
+    mean_wind_speed + bright_sunshine + mean_pressure,
+  
+  sigma.formula = ~ mean_temp + mean_relative_hum +
+    mean_wind_speed + bright_sunshine,
+  
+  nu.formula = ~ mean_temp + mean_relative_hum +
+    mean_pressure + mean_wind_speed,
+  
+  family = ZAGA,
+  data = df
+)
+
+wp(zaga2, xvar = df$mean_temp)
+wp(zaga2, xvar = df$mean_relative_hum)
+plot(zaga2)
+GAIC(zaga, zaga2)
