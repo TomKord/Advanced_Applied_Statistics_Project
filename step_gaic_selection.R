dbc76715205f84dcbf67c47ceecdf7fa07f8a3c4
Thomas-cj <119956954+Thomas-cj@users.noreply.github.com>
2025-12-28 14:53:37 +0100

    added model testing + png output, added final cov pen pipeline, remnoved last row of df due to uncecessary date

--- step_gaic_selection.R
+++ step_gaic_selection.R
@@ -4,13 +4,12 @@ df <- read.csv("Weather_data.csv")
 
 df$date <- as.Date(df$date)
 df$time_num <- as.numeric(df$date)
-
-df <- subset(df, df$acc_precip < 50)
+df$time_index <- seq_len(nrow(df))
 
 possible_vars <- ~ mean_temp + mean_relative_hum + mean_pressure + 
   bright_sunshine + mean_wind_speed + 
   I(mean_temp^2) + I(mean_relative_hum^2) + 
-  mean_temp:mean_relative_hum
+  mean_temp:mean_relative_hum + time_index
 
 m_null <- gamlss(acc_precip ~ 1, 
                  data = df, 
