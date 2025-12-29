cc6b88dbcd38ac5d83166160c7fe60a996505c02
TomKord <tom.kordes1@gmail.com>
2025-12-29 14:10:06 +0100

    Correcting stepGAIC

--- stepGAIC.R
+++ stepGAIC.R
@@ -6,12 +6,11 @@ df$date <- as.Date(df$date)
 df$time_num <- as.numeric(df$date)
 df$time_index <- seq_len(nrow(df))
 
-df <- subset(df, df$acc_precip < 50)
 
 possible_vars <- ~ mean_temp + mean_relative_hum + mean_pressure + 
   bright_sunshine + mean_wind_speed + 
   I(mean_temp^2) + I(mean_relative_hum^2) + 
-  mean_temp:mean_relative_hum
+  mean_temp:mean_relative_hum + time_index
 
 m_null <- gamlss(acc_precip ~ 1, 
                  data = df, 
